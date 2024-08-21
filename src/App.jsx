5f9570a41711a4810d9673ccc404d89392c8d93e
Fariba Shamseh <fariba.shamseh0@gmail.com>
2024-08-21 20:32:20 +0430

    Add hover effect to stars

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ const starStyle = {
 
 export default function App({maxRating = 3}) {
     const [Rating, setRating] = useState(1);
+    const [isHover, setIsHover] = useState(1);
 
     function handleRate(rate) {
         setRating(rate);
@@ -35,17 +36,18 @@ export default function App({maxRating = 3}) {
         <div style={containerStyle}>
             <div style={starContainerStyle}>
                 {Array.from({length: maxRating}, (_, i) => (
-                    <Star key={i} onRate={() => handleRate(i + 1)} full={Rating >= i + 1}/>
+                    <Star key={i} onRate={() => handleRate(i + 1)} full={isHover >= i + 1}
+                          mouseEnter={() => setIsHover(i + 1)} mouseLeave={() => setIsHover(0)}/>
                 ))}
             </div>
-            <p style={textStyle}>{Rating}</p>
+            <p style={textStyle}>{isHover}</p>
         </div>
     );
 }
 
-function Star({onRate, full}) {
+function Star({onRate, full, mouseEnter, mouseLeave}) {
     return (
-        <span role="button" style={starStyle} onClick={onRate}>
+        <span role="button" style={starStyle} onClick={onRate} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
       {full ? (<svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
@@ -68,8 +70,6 @@ function Star({onRate, full}) {
                   d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
               />
           </svg>)}
-
-
     </span>
     );
 }
